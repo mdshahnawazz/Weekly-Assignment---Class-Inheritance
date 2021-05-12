9f45c68011b4a3a5ab962b55fa80305eadaca5dc
mdshahnawazz <70908310+mdshahnawazz@users.noreply.github.com>
2021-05-12 20:56:15 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -4,7 +4,17 @@
 
 
   
-class Shape {  constructor(color) {    this.color = color;  }  calculateArea() {    console.log(this.constructor.name + ' Area Calclated Called');  }  drawShape() {    return '';  }
-}module.exports = Shape;//  //export default Shape
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  calculateArea() {
+    console.log(this.constructor.name + ' Area Calclated Called');
+  }
+  drawShape() {
+    return '';
+  }
 
-
+}
+module.exports = Shape;
+//  //export default Shape
