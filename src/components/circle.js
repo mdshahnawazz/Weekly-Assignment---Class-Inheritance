c1caee6e9289eb4a4545caa6252e276e510f6081
mdshahnawazz <70908310+mdshahnawazz@users.noreply.github.com>
2021-05-12 20:38:28 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -5,3 +5,14 @@
 
 // export class using module.exports
 
+import React, { Component } from 'react';
+export class App extends Component {    render() {
+        return <h1>Hello</h1>;    }
+}
+export default App;
+
+app.js
+import Shape from './shape';
+class Circle extends Shape {  calculateArea() {    super.calculateArea();    console.log(this.constructor.name + ' Area Calclated Called');  }}module.exports = Circle;
+
+circle.js
